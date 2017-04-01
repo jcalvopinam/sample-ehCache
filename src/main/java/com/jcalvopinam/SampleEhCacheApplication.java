1cab873bfeed898fa3ebc2713ad94b0b2558e9fe
Juan Calvopina <juan.calvopina+dev@gmail.com>
2017-04-01 20:00:20 -0300

    Project renamed

--- src/main/java/com/jcalvopinam/SampleEhCacheApplication.java
+++ src/main/java/com/jcalvopinam/SampleEhCacheApplication.java
@@ -2,11 +2,14 @@ package com.jcalvopinam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PocEhCacheApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PocEhCacheApplication.class, args);
     }
+
 }
